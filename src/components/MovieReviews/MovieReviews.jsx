f0d1bb270150f15da4df1abb1ff88be46558c61f
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-08-20 03:16:07 +0200

    add movie reviews

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -1,7 +1,45 @@
 // import css from './MovieReviews.module.css'
 
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchMovieReviews } from "../../service/movieApi";
+
 function MovieReviews() {
-  return <div>MovieReviews</div>;
+  const { movieId } = useParams();
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const gethMovieReviews = async () => {
+      try {
+        const dataReviews = await fetchMovieReviews(movieId);
+        console.log(dataReviews);
+
+        setReviews(dataReviews.results);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    gethMovieReviews();
+  }, [movieId]);
+
+  return (
+    <div>
+      {reviews && reviews.length > 0 ? (
+        <ul>
+          {reviews.map((review) => {
+            return (
+              <li key={review.id}>
+                <h4>{review.author}</h4>
+                <p>{review.content}</p>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p></p>
+      )}
+    </div>
+  );
 }
 
 export default MovieReviews;
